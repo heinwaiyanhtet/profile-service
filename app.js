30cacb274eda885423f224001310d9f6d2af0111
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-12-24 20:46:37 +0630

    gitignore added

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 
 const cors = app.use("cors");
 const corsOptions = require('./config/corsOptions');
-
-
 app.use(cors(corsOptions));
+
+
+
+
+
+
+
+
+
 const PORT = process.env.PORT || 4000;
-
-
-
 app.listen(PORT,
     () => console.log(`Server is running on port 4000 ${PORT}`));
